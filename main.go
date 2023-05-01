fa55f52b40376f3d445726f70709c074a36f117e
frort <87010782+kienn-HCl@users.noreply.github.com>
2023-05-01 11:59:43 +0900

    dispay on the screen

--- main.go
+++ main.go
@@ -8,20 +8,38 @@ import (
 	"golang.org/x/term"
 )
 
-func printRow(row []bool, width int) {
-	for i := 0; i < width; i++ {
-		if row[i] {
-			print(" ")
-		} else {
+func printRow(row []bool) {
+	for _, i := range row {
+		if i {
 			print("#")
+		} else {
+			print(" ")
 		}
 	}
 }
 
-func ruleResult(cells int8, rule int8) bool {
+func ruleResult(cells [3]bool, rule int8) bool {
     n := 0
-    for i:=0; i<3; i++ {
+    for i:=2; i>=0; i-- {
+        n = n << 1
+        if cells[i] {
+            n+=1
+        } 
     }
+
+    return (rule >> n) & 0b1 == 1
+}
+
+func updateRow(row []bool, rule int8, width int) []bool {
+    update := make([]bool, width, width)
+
+    update[0] = ruleResult([3]bool{row[width-1],row[0],row[1]}, rule)
+    update[width-1] = ruleResult([3]bool{row[width-2],row[width-1],row[0]}, rule)
+    for i:=1; i<width-1; i++ {
+        update[i] = ruleResult([3]bool(row[i-1:i+2]), rule)
+    }
+
+    return update
 }
 
 func main() {
@@ -33,4 +51,10 @@ func main() {
 
 	row := make([]bool, width, width)
 	row[width/2] = true
+
+    printRow(row)
+    for i:= 0; i<(width/2); i++ {
+        row = updateRow(row, 90, width)
+        printRow(row)
+    }
 }
